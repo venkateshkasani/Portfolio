0fd1310ba4802573c8bbb50b50f623f40e52555f
Venkatesh <venkateshkasani14@gmail.com>
2024-12-06 15:19:35 +0530

    minor-correction

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Dummy from './sections/Dummy';
 import Homepage from './sections/Homepage';
 import { Routes,Route } from 'react-router-dom';
 
@@ -9,7 +8,6 @@ const App:React.FC = () => {
     <div className='text-white bg-black w-[100vw] min-h-[100vh] h-full'>
       <Routes>
         <Route path='/' element={<Homepage/>} />
-        <Route path='/dummy' element={<Dummy />} />
       </Routes>
       </div>
     </>
